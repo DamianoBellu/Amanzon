8771be8bb9ad5df3c8b189fa3eabf5387864234a
Damiano Bellu <belludamiano9@gmail.com>
2023-09-28 16:52:30 +0200

    Amazon 2

--- src/app/Services/carrello.service.ts
+++ src/app/Services/carrello.service.ts
@@ -1,15 +1,48 @@
 import { Injectable } from '@angular/core';
-import { Prodotto } from '../Models/prodotto';
+import { Prodotto, ProdottoACarrello } from '../Models/prodotto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarrelloService {
-  prodotti: Prodotto[] = [];
+  prodotti: ProdottoACarrello[] = [];
+  totale = 0;
 
   constructor() { }
 
-  aggiungi(prodotto: Prodotto){
+  aggiungi(prodotto: ProdottoACarrello){
+    let p = this.prodotti.find(x => x.id == prodotto.id);
+
+    if(p){
+      p.qta ++;
+    }else{
+       
     this.prodotti.push(prodotto);
+    }
+   
+    this.calcolaTotale();
+  }
+
+  svuota() {
+    this.prodotti = [];
+    this.calcolaTotale();
+  }
+
+  calcolaTotale() {
+    this.totale = 0;
+    for(const p of this.prodotti){
+      this.totale += (p.price * p.qta);
+    }
+  }
+
+  rimuovi(prodottoId: number){
+    const p = this.prodotti.find(x => x.id == prodottoId);
+
+    if(p){
+      const i = this.prodotti.indexOf(p);
+      this.prodotti.splice(i, 1);
+
+      this.calcolaTotale();
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdottiVetrinaComponent } from './Components/prodotti-vetrina/prodotti-vetrina.component';
 import { ProdottoPreviewComponent } from './Components/prodotto-preview/prodotto-preview.component';
+import { CarrelloComponent } from './Components/carrello/carrello.component';
+import { CarrelloMiniComponent } from './Components/carrello-mini/carrello-mini.component';
+import { HeaderComponent } from './Components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProdottiVetrinaComponent,
-    ProdottoPreviewComponent
+    ProdottoPreviewComponent,
+    CarrelloComponent,
+    CarrelloMiniComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
